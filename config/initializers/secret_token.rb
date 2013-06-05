6e13826f0af33ba12d946d65d1a75720742538a6
Jon Gillies <jon@gillies.us>
2013-06-05 10:24:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snarf::Application.config.secret_token = 'b741ff92f1b53f798b65e89a1ac60b1576185128a4254022115dd8f5e8c1a2468f49473f5fc0ede8df9ab8a638669668b5f9bcdb5c90d746c58b313b0b264edb'
