f840a73d8d5f5ffd4624b763e4dd98e61a735f13
Jon Gillies <jon@gillies.us>
2013-06-05 13:49:23 -0700

    routing on path

---
+++ app/api/api_v2.rb
@@ -0,0 +1,10 @@
+class ApiV2 < Grape::API
+  prefix 'api'
+  version 'v2' #, using: :header, vendor: 'twitter'
+  format :json
+
+  mount Acme::Ping2
+  mount Acme::Raise
+  mount Acme::Protected
+  mount Acme::Post
+end
